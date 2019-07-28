831106d324286b8601a476582fed96f6c65823ab
edwardhuahan <me.edhead@gmail.com>
2019-07-28 17:20:14 -0600

    solutions
    New code

---
+++ threenplusone.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    long long int i, j;
+    cin >> i >> j;
+
+    // n loops 
+    for (int n = 0; n < j; n++) {
+        
+    }
+    return 0;
+}
